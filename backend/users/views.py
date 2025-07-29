6e1d1c7dc1f3368f6ad995237bf5a774266736f2
Владимир Ли <vovalee@MacBook-Pro-Vladimir.local>
2025-07-29 17:13:08 +0400

    4

--- backend/users/views.py
+++ backend/users/views.py
@@ -4,14 +4,14 @@ import os
 from django.contrib.auth import get_user_model
 from django.core.files.base import ContentFile
 from djoser.views import UserViewSet
+from recipes.paginations import PageLimitPagination
+from recipes.serializers import FavoriteSerializer
 from rest_framework import permissions, status
 from rest_framework.decorators import action
 from rest_framework.generics import get_object_or_404
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 
-from recipes.paginations import PageLimitPagination
-from recipes.serializers import FavoriteSerializer
 from .models import Subscribe
 from .serializers import AvatarSerializer
 
